1857d02008266257e04c9e0c982d48264ab50c07
Marvin Biesenbach <marvin.biesenbach@smail.th-koeln.de>
2018-12-21 23:51:21 +0100

    init

---
+++ OSMWebApi/Models/OSMTag.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSMWebApi.Models
+{
+    public class OSMTag
+    {
+        [BsonElement("k")]
+        public string key;
+
+        [BsonElement("v")]
+        public string value;
+    }
+}
